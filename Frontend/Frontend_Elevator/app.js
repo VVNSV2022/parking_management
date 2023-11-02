34a120ff25b8dc837e87540a459953db1fd9b460
RohithGummadi <rohithmc.gummadi@gmail.com>
2023-11-02 16:44:21 -0400

    Added Comments for Elevator Frontend

--- Frontend/Frontend_Elevator/app.js
+++ Frontend/Frontend_Elevator/app.js
@@ -2,7 +2,7 @@ function handleLicensePlateButtonClick() {
     const licensePlate = prompt("Enter your License Plate Number:");
 
     if (licensePlate) {
-        // Make an API request to check the license plate
+        // Making an API request to check the license plate
         fetch('/api/elevator/scan-license-plate', {
             method: 'POST',
             headers: {
@@ -12,14 +12,14 @@ function handleLicensePlateButtonClick() {
         })
         .then(response => {
             if (response.status === 200) {
-                // Reservation found, show success message and redirect to home.html
+                // When reservation found, show success message and redirect to Reservations Page
                 return response.json()
                     .then(data => {
                         alert(data.message);
                         window.location.href = 'home.html';
                     });
             } else if (response.status === 404) {
-                // No reservation found, show not found message and redirect to notFound.html
+                // When no reservation found, show not found message and redirect to notFound.html
                 return response.json()
                     .then(data => {
                         alert(data.message);
@@ -35,7 +35,7 @@ function handleLicensePlateButtonClick() {
     }
 }
 
-// Call the rendering functions when the page loads
+// Calling the rendering functions when the page loads
 window.onload = function () {
     const licensePlateButton = document.getElementById("licensePlateButton");
     licensePlateButton.addEventListener("click", handleLicensePlateButtonClick);
