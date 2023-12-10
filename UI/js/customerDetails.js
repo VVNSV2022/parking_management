1ce89c82530323f615c4637970457f129c5230f6
Sri Harsha Samayamanthula <ss4305@scarletmail.rutgers.edu>
2023-12-10 17:30:24 -0500

    styling

--- UI/js/customerDetails.js
+++ UI/js/customerDetails.js
@@ -1,5 +1,5 @@
 localStorage.setItem('userID','12345678');
-localStorage.setItem('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c2VyLTE3MDExODExNTU3ODgiLCJpYXQiOjE3MDIyNDAxMTIsImV4cCI6MTcwMjI0MTAxMn0.7bTP9q-sn-Btf3TV2zuAuJt2ePvTUxAhVQx-OvMU19g')
+localStorage.setItem('token','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c2VyLTE3MDExODExNTU3ODgiLCJpYXQiOjE3MDIyNDczNTYsImV4cCI6MTcwMjI0ODI1Nn0.pvpFszY-NvFGLT3wTfzJzrHErXFYcwK-ZmzfCQW571U')
 document.addEventListener("DOMContentLoaded", function() {
   const contentDiv = document.getElementById('content');
   const profileLink = document.getElementById('profileLink');
@@ -14,41 +14,54 @@ document.addEventListener("DOMContentLoaded", function() {
       const response = await fetch(`/api/customer?userID=${userID}`);
       const userData = await response.json();
 
-          contentDiv.innerHTML = `
-          <div class="personal-details-container">
-            <h2 class="personal-details-heading">Personal Details</h2>
-            <div class="user-details">
-              <div class="user-detail">
-                <strong>Username:</strong> ${userData.username}
-              </div>
-              <div class="user-detail">
-                <strong>Email:</strong> ${userData.email}
-              </div>
-              <div class="user-detail">
-                <strong>Date of Birth:</strong> ${userData.dateOfBirth}
-              </div>
-              <div class="user-detail">
-                <strong>Is Disabled:</strong> ${userData.isDisabled}
-              </div>
-              
-              <h2 class="address-heading">Current Address</h2>
-              <div class="user-detail">
-                <strong>Street:</strong> ${userData.address.streetNumber}
-              </div>
-              <div class="user-detail">
-                <strong>City:</strong> ${userData.address.city}
-              </div>
-              <div class="user-detail">
-                <strong>Apt Unit:</strong> ${userData.address.aptUnit}
-              </div>
-              <div class="user-detail">
-                <strong>State:</strong> ${userData.address.state}
-              </div>
+          
+      contentDiv.innerHTML = `
+            <div class="personal-details-container">
+                <h2 class="personal-details-heading">Personal Details</h2>
+                <div class="user-details">
+                    <div class="user-detail">
+                        <strong>Username:</strong> <span>${userData.username}</span>
+                    </div>
+                    <div class="user-detail">
+                        <strong>Email:</strong> <span>${userData.email}</span>
+                    </div>
+                    <div class="user-detail">
+                        <strong>Date of Birth:</strong> <span>${userData.dateOfBirth}</span>
+                    </div>
+                    <div class="user-detail">
+                        <strong>Is Disabled:</strong> <span>${userData.isDisabled}</span>
+                    </div>
+                    
+                    <h2 class="address-heading">Current Address</h2>
+                    <div class="user-detail">
+                        <strong>Street:</strong> <span>${userData.address.streetNumber}</span>
+                    </div>
+                    <div class="user-detail">
+                        <strong>City:</strong> <span>${userData.address.city}</span>
+                    </div>
+                    <div class="user-detail">
+                        <strong>Apt Unit:</strong> <span>${userData.address.aptUnit}</span>
+                    </div>
+                    <div class="user-detail">
+                        <strong>State:</strong> <span>${userData.address.state}</span>
+                    </div>
+                </div>
             </div>
-          </div>
-          <button class="update-btn">Update</button>
+            <button class="update-btn">Update</button>
         `;
 
+        const userDetailItems = document.querySelectorAll('.user-detail');
+        userDetailItems.forEach(item => {
+            item.style.display = 'flex';
+            item.style.alignItems = 'center';
+            item.style.marginBottom = '10px';
+        });
+
+        const userDetailLabels = document.querySelectorAll('.user-detail strong');
+        userDetailLabels.forEach(label => {
+            label.style.minWidth = '120px'; // Set a minimum width for labels for uniform alignment
+            label.style.marginRight = '10px'; // Add some margin between label and value
+        });
         const personalDetailsContainer = document.querySelector('.personal-details-container');
         personalDetailsContainer.style.backgroundColor = '#6b6b9e';
         personalDetailsContainer.style.color = '#fff';
@@ -64,7 +77,7 @@ document.addEventListener("DOMContentLoaded", function() {
         addressHeading.style.marginTop = '30px'; // Adjust spacing between sections
 
         const updateButton = document.querySelector('.update-btn');
-        updateButton.style.backgroundColor = '#6b6b9e';
+        // updateButton.style.backgroundColor = '#6b6b9e';
         updateButton.style.color = '#fff';
         updateButton.style.border = 'none';
         updateButton.style.borderRadius = '5px';
@@ -95,7 +108,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         const membershipResult = await membershipResponse.json();
         const membershipData = membershipResult.data;
-        console.log(membershipData[0].startDate);
+
         let membershipInfo = '';
         if (membershipData && membershipData.length > 0) {
             membershipInfo = `
@@ -122,22 +135,22 @@ document.addEventListener("DOMContentLoaded", function() {
                         return `
                             <div class="membership-box">
                                 <div class="membership-detail">
-                                    <strong>Membership ID:</strong> ${membership.membershipID}
+                                    <strong>Membership ID:</strong> <span>${membership.membershipID}</span>
                                 </div>
                                 <div class="membership-detail">
-                                    <strong>Membership Type:</strong> ${membership.membershipType}
+                                    <strong>Membership Type:</strong> <span>${membership.membershipType}</span>
                                 </div>
                                 <div class="membership-detail">
-                                    <strong>Membership Period:</strong> ${membership.membershipPeriod}
+                                    <strong>Membership Period:</strong> <span>${membership.membershipPeriod}</span>
                                 </div>
                                 <div class="membership-detail">
-                                    <strong>Start Date:</strong> ${formattedStartDate}
+                                    <strong>Start Date:</strong> <span>${formattedStartDate}</span>
                                 </div>
                                 <div class="membership-detail">
-                                    <strong>End Date:</strong> ${formattedEndDate}
+                                    <strong>End Date:</strong> <span>${formattedEndDate}</span>
                                 </div>
                                 <div class="membership-detail">
-                                    <strong>Region ID:</strong> ${membership.regionID}
+                                    <strong>Region ID:</strong> <span>${membership.regionID}</span>
                                 </div>
                             </div>
                         `;
@@ -153,41 +166,38 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-
   vehiclesLink.addEventListener('click', async function(event) {
     event.preventDefault();
     try {
-      const userID = localStorage.getItem('userID');
-      const token = localStorage.getItem('token');
-      const vehiclesResponse = await fetch(`/api/vehicles?userID=${userID}`,{
-        headers: {
+        const userID = localStorage.getItem('userID');
+        const token = localStorage.getItem('token');
+        const vehiclesResponse = await fetch(`/api/vehicles?userID=${userID}`,{
+            headers: {
                 'Authorization': `Bearer ${token}`
-        },
-      });
-      const vehiclesResult = await vehiclesResponse.json();
-      const vehiclesData=vehiclesResult.data;
-      console.log(vehiclesData);
+            },
+        });
+        const vehiclesResult = await vehiclesResponse.json();
+        const vehiclesData = vehiclesResult.data;
 
-      // Display vehicles data
-       let vehiclesInfo = '';
+        let vehiclesInfo = '';
         if (vehiclesData && vehiclesData.length > 0) {
-           vehiclesInfo = vehiclesInfo = `
+            vehiclesInfo = `
                 <div class="vehicle-details-container">
                     <h2 class="vehicle-details-heading">Vehicles</h2>
                     <div class="vehicle-details-content">
                         ${vehiclesData.map(vehicle => `
                             <div class="vehicle-detail">
                                 <div class="vehicle-info">
-                                    <strong>Vehicle Make:</strong> ${vehicle.vehicleMake}
+                                    <span><strong>Vehicle Make:</strong> ${vehicle.vehicleMake}</span>
                                 </div>
                                 <div class="vehicle-info">
-                                    <strong>Vehicle Model:</strong> ${vehicle.vehicleModel}
+                                    <span><strong>Vehicle Model:</strong> ${vehicle.vehicleModel}</span>
                                 </div>
                                 <div class="vehicle-info">
-                                    <strong>License Plate Number:</strong> ${vehicle.licensePlateNumber}
+                                    <span><strong>License Plate Number:</strong> ${vehicle.licensePlateNumber}</span>
                                 </div>
                                 <div class="vehicle-info">
-                                    <strong>Vehicle Color:</strong> ${vehicle.vehicleColor}
+                                    <span><strong>Vehicle Color:</strong> ${vehicle.vehicleColor}</span>
                                 </div>
                                 <button class="delete-btn" data-vehicle-id="${vehicle.vehicleID}">Delete</button>
                             </div>
@@ -196,84 +206,79 @@ document.addEventListener("DOMContentLoaded", function() {
                 </div>
                 <button class="add-btn">Add</button>`;
         } else {
-          vehiclesInfo = `<p>No vehicles found</p>
-            <div class="vehicle-buttons">
-              <button class="add-btn">Add</button>
-            </div>`;
+            vehiclesInfo = `<p>No vehicles found</p>
+                <div class="vehicle-buttons">
+                    <button class="add-btn">Add</button>
+                </div>`;
         }
         contentDiv.innerHTML = vehiclesInfo;
 
-        const vehicleDetailsContainer = document.querySelector('.vehicle-details-container');
-        // vehicleDetailsContainer.style.backgroundColor = '#6b6b9e';
-        // vehicleDetailsContainer.style.color = '#fff';
-        vehicleDetailsContainer.style.borderRadius = '10px';
-        vehicleDetailsContainer.style.padding = '20px';
-        vehicleDetailsContainer.style.textAlign = 'center';
+        const vehicleDetailsContent = document.querySelector('.vehicle-details-content');
+        vehicleDetailsContent.style.display = 'flex';
+        vehicleDetailsContent.style.flexWrap = 'wrap';
+        vehicleDetailsContent.style.justifyContent = 'center';
 
-        const vehicleDetailsHeading = document.querySelector('.vehicle-details-heading');
-        vehicleDetailsHeading.style.marginTop = '0';
-        vehicleDetailsHeading.style.fontSize = '24px';
-        vehicleDetailsHeading.style.backgroundColor = '#8a8acc';
-        vehicleDetailsHeading.style.color = '#fff';
-        vehicleDetailsHeading.style.borderRadius = '8px';
-        vehicleDetailsHeading.style.padding = '10px 20px';
-        vehicleDetailsHeading.style.textAlign = 'center';
-        vehicleDetailsHeading.style.display = 'inline-block';
-        vehicleDetailsHeading.style.border = '2px solid #8a8acc';
-
-        const addButton = document.querySelector('.add-btn');
-        addButton.style.backgroundColor = '#6b6b9e';
-        addButton.style.color = '#fff';
-        addButton.style.border = 'none';
-        addButton.style.borderRadius = '5px';
-        addButton.style.padding = '10px 20px';
-        addButton.style.cursor = 'pointer';
-        addButton.style.transition = 'background-color 0.3s';
-        addButton.style.marginTop = '20px';
-
-        addButton.addEventListener('click', function() {
-          window.location.href = '../screens/addVehicle.html';
+        const vehicleInfos = document.querySelectorAll('.vehicle-info span');
+        vehicleInfos.forEach(info => {
+            info.style.display = 'block';
+            info.style.border = '1px solid #ccc';
+            info.style.borderRadius = '5px';
+            info.style.padding = '8px';
+            info.style.marginBottom = '5px';
+            info.style.width = '100%';
         });
 
         const deleteButtons = document.querySelectorAll('.delete-btn');
         deleteButtons.forEach(button => {
-          button.style.display = 'block';
-          button.style.marginTop = '10px';
-          button.style.backgroundColor = '#8a8acc';
-          button.style.color = '#fff';
-          button.style.border = 'none';
-          button.style.borderRadius = '5px';
-          button.style.padding = '8px 16px';
-          button.style.cursor = 'pointer';
-          button.style.transition = 'background-color 0.3s';
+            button.style.display = 'block';
+            button.style.marginTop = '10px';
+            button.style.border = 'none';
+            button.style.borderRadius = '5px';
+            button.style.padding = '8px 16px';
+            button.style.cursor = 'pointer';
+            button.style.transition = 'background-color 0.3s';
         });
 
         deleteButtons.forEach(deleteButton => {
-          deleteButton.addEventListener('click', async function() {
-            const vehicleID = deleteButton.getAttribute('data-vehicle-id');
-            try {
-              const response = await fetch(`/api/vehicle?vehicleID=${vehicleID}&userID=${userID}`, {
-                headers: {
-                  'Authorization': `Bearer ${token}`
-                },
-                method: 'DELETE'
-              });
-              if (response.ok) {
-                deleteButton.closest('.vehicle-detail').style.display = 'none';
-              } else {
-                console.error('Failed to delete vehicle');
-              }
-            } catch (error) {
-              console.error('Error deleting vehicle:', error);
-            }
-          });
+            deleteButton.addEventListener('click', async function() {
+                const vehicleID = deleteButton.getAttribute('data-vehicle-id');
+                try {
+                    const response = await fetch(`/api/vehicle?vehicleID=${vehicleID}&userID=${userID}`, {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        },
+                        method: 'DELETE'
+                    });
+                    if (response.ok) {
+                        deleteButton.closest('.vehicle-detail').style.display = 'none';
+                    } else {
+                        console.error('Failed to delete vehicle');
+                    }
+                } catch (error) {
+                    console.error('Error deleting vehicle:', error);
+                }
+            });
         });
 
-      } catch (error) {
+        const addButton = document.querySelector('.add-btn');
+        addButton.style.marginTop = '20px';
+        addButton.style.padding = '10px 20px';
+        addButton.style.border = 'none';
+        addButton.style.borderRadius = '5px';
+        addButton.style.backgroundColor = '#6b6b9e';
+        addButton.style.color = 'white';
+        addButton.style.cursor = 'pointer';
+        addButton.style.transition = 'background-color 0.3s';
+
+        addButton.addEventListener('click', function() {
+            window.location.href = '../screens/addVehicle.html';
+        });
+
+    } catch (error) {
         console.error('Error fetching vehicles:', error);
         contentDiv.innerHTML = '<p>Error fetching vehicles. Please try again later.</p>';
-      }
-    });
+    }
+});
 
   paymentmethodsLink.addEventListener('click', async function(event) {
     event.preventDefault();
